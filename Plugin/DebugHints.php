d02c4d1fe2b519ad09c8462502aa08d4d410ef3b
Antonio Vitale <antoniovitale@dnafactory.it>
2021-02-02 14:11:29 +0100

    Aggiornamento 2.0.0
    + Compatibilità con Magento 2.4.*

--- Plugin/DebugHints.php
+++ Plugin/DebugHints.php
@@ -18,33 +18,52 @@ use Magento\Framework\App\Request\Http;
  */
 class DebugHints extends \Magento\Developer\Model\TemplateEngine\Plugin\DebugHints
 {
-
-    protected $_httpRequest;
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected ScopeConfigInterface $_scopeConfig;
+    /**
+     * @var StoreManagerInterface
+     */
+    protected StoreManagerInterface $_storeManager;
+    /**
+     * @var DebugHintsFactory
+     */
+    protected DebugHintsFactory $_debugHintsFactory;
+    /**
+     * @var Http
+     */
+    protected Http $_http;
 
     public function __construct(
         ScopeConfigInterface $scopeConfig,
         StoreManagerInterface $storeManager,
         DevHelper $devHelper,
         DebugHintsFactory $debugHintsFactory,
+        Http $http,
         $debugHintsPath,
-        Http $httpRequest
+        $debugHintsWithParam = null,
+        $debugHintsParameter = null
     ) {
-        $this->_httpRequest = $httpRequest;
-        parent::__construct($scopeConfig, $storeManager, $devHelper, $debugHintsFactory, $debugHintsPath);
+        $this->_scopeConfig = $scopeConfig;
+        $this->_storeManager = $storeManager;
+        $this->_debugHintsFactory = $debugHintsFactory;
+        $this->_http = $http;
+        parent::__construct($scopeConfig, $storeManager, $devHelper, $debugHintsFactory, $http, $debugHintsPath, $debugHintsWithParam, $debugHintsParameter);
     }
 
     public function afterCreate(
         TemplateEngineFactory $subject,
         TemplateEngineInterface $invocationResult
     ) {
-        $storeCode = $this->storeManager->getStore()->getCode();
-        if ($this->_httpRequest->getParam('debug', 'no') == 'yes') {
-            $showBlockHints = $this->scopeConfig->getValue(
+        $storeCode = $this->_storeManager->getStore()->getCode();
+        if ($this->_http->getParam('debug', 'no') == 'yes') {
+            $showBlockHints = $this->_scopeConfig->getValue(
                 self::XML_PATH_DEBUG_TEMPLATE_HINTS_BLOCKS,
                 ScopeInterface::SCOPE_STORE,
                 $storeCode
             );
-            return $this->debugHintsFactory->create([
+            return $this->_debugHintsFactory->create([
                 'subject' => $invocationResult,
                 'showBlockHints' => $showBlockHints,
             ]);
